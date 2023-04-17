d018e6e2093b12d2cb453d34a9f9bffa38f0e405
Spectrum5 <toskusergio@gmail.com>
2023-04-17 09:54:33 +0200

    add store e import axios

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,26 @@
-<!--
-    Per passare da Composition API a Options API dobbiamo:
-    1. Rimuovere l'attributo setup da <script>
-    2. Aggiungere l'export default {}
-    3. Aggiungere la proprietà name nell'oggetto esportato che avrà come valore il nome del componente (opzionale, ma buona pratica)
--->
 <script>
 import { store } from './store';
-
-// Passi per utilizzare sottocomponenti
-// 1. Importazione
-
 export default {
-    name: "App",
+    name: 'App',
     data() {
         return {
             store
         }
     },
-    // 2. Registrazione
     components: {
-        
-    },
-    methods: {
-        
     }
-};
+}
 </script>
 
 <template>
-    <!-- 3. Utilizzo (nuovo tag HTML self-closing) -->
-
+    <div class="container text-center">
+        <h1>Test</h1>
+        <button class="btn btn-primary">test</button>
+        <div>{{ store.test }}</div>
+    </div>
 </template>
 
-<!-- Nel componente App.vue, rimuoviamo anche l'attributo scoped su <style> -->
-<!-- E se voglio utilizzare sass, aggiungo l'attributo lang="scss" -->
 <style lang="scss">
-// Qui possiamo anche importare dei file .scss (attenzione al percorso)
 @import './styles/main';
 
 </style>
