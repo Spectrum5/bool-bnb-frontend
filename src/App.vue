9e8474f1d2cc5a4483750fbb4f47144a4d8c5890
Spectrum5 <toskusergio@gmail.com>
2023-04-17 10:13:57 +0200

    add style per prova

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,16 @@ export default {
 
 <template>
     <div class="container text-center">
-        <h1>Test</h1>
-        <button class="btn btn-primary">test</button>
+        <h1>Prova h1</h1>
+        <h2>Prova h2 <span class="title-bnb">testo evidenziato</span>
+        </h2>
+        <h3>prova testo 3</h3>
+        <button class="button">
+            Vedi tutti
+        </button>
         <div>{{ store.test }}</div>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo ut quae voluptate nihil officiis incidunt iure
+            neque inventore sequi omnis ipsum obcaecati totam, perspiciatis necessitatibus corrupti.</p>
     </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import {
     reactive
-} from 'vue'
-import axios from 'axios';
-// import axios per fare call api
+} from 'vue';
 
-export const store = reactive ({
-    test : 'store.js funziona'
+// import axios per fare call api
+import axios from 'axios';
+
+
+export const store = reactive({
+    test: 'store.js funziona'
 })
