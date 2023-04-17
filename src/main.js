037b0ab31a6ea2637ff085f33bd246bd32a756c2
CalendaLucia <calendalucia4@gmail.com>
2023-04-17 21:43:13 +0200

    navbar creata

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
 import { createApp } from 'vue'
-// Nel caso in cui volessimo usare SASS, è consigliabile rimuovere l'importazione del file style.css da qui (e cancellarlo eventualmente)
-// Non è obbligatorio, ma potremmo creare inutili conflitti
-// import './style.css'
 import App from './App.vue'
-// Import router
-import { router } from './router';
+import { router } from './router'
 
-createApp(App).use(router).mount('#app');
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
+
+import { faWaterLadder } from '@fortawesome/free-solid-svg-icons'
+import { faWifi } from '@fortawesome/free-solid-svg-icons'
+import { faCarSide } from '@fortawesome/free-solid-svg-icons'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
+import { faTv } from '@fortawesome/free-solid-svg-icons'
+import { faSnowflake } from '@fortawesome/free-solid-svg-icons'
+import { faUserLock } from '@fortawesome/free-solid-svg-icons'
+import {  faBroom } from '@fortawesome/free-solid-svg-icons'
+import { faSoap} from '@fortawesome/free-solid-svg-icons'
+import { faPaw } from '@fortawesome/free-solid-svg-icons'
+import {  faVault } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faBars,faCircleUser,faMagnifyingGlass,faGlobe,faWaterLadder,faWifi,faCarSide,faUtensils,faTv,faSnowflake,faUserLock,faBroom,faSoap,faPaw,faVault)
+
+createApp(App)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')
--- src/views/public/HomeView.vue
+++ src/views/public/HomeView.vue
@@ -4,6 +4,8 @@
 import axios from 'axios';
 // import {router} from '../../router';
 
+
+
 export default {
     name: 'HomeView',
     data() {
@@ -11,7 +13,24 @@ export default {
             // router,
             currentPage: 1,
             searchTitle: '',
-            apartments: []
+            apartments: [],
+            menuItems: [
+                { label: 'Ovunque', link: '#' },
+                { label: '01 giu - 31 ago', link: '#' },
+                { label: '1 ospite', link: '#' },
+                
+            ],
+
+            menuItems2: [
+                 { label: 'Affitta con Airbnb', link: '#' },
+                 { icon: 'fa-solid fa-globe', link: '#' },
+            ],
+               menuHamb: [
+                   { icon: 'fa-solid fa-bars', link: '#' },
+                   { icon: 'fa-solid fa-circle-user', link: '#' }
+            ],
+
+            
         }
     },
     methods: {
@@ -45,6 +64,53 @@ export default {
 
 <template>
 
+    <header>
+        <nav class="navbar">
+            <div class="container-fluid">
+               <div class="logo-container">
+                  <img src="../../assets/img/logo.png" alt="logo">
+                  <h2>BollBnb</h2>
+               </div>
+
+               <div class="searchbar">
+                  <ul class="group-list">
+                     <li class="item" v-for="(item, index) in menuItems" :key="index">
+                       <a :href="item.link">{{ item.label }}</a>
+                       <i class="line"></i>
+                     </li>
+                     <li class="item icon">
+                        <a href="#" >
+                           <font-awesome-icon class="myicon" icon="fa-solid fa-magnifying-glass"/>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+
+               <div class="right-menu">
+                  <ul class="group-list">
+                    <li class="item" v-for="item in menuItems2" :key="item.label">
+                        <a :href="item.link">{{ item.label }}</a>
+                    </li>
+                    <li class="item">
+                        <font-awesome-icon :icon="menuItems2[1].icon" class="globe"/>
+                    </li>
+                  </ul>
+                  <ul class="group-list">
+                    <li class="item d-inline-block">
+                       <font-awesome-icon :icon="menuHamb[0].icon" class="bars"/>
+                    </li>
+                    <li class="item d-inline-block">
+                       <font-awesome-icon :icon="menuHamb[1].icon" class="user"/>
+                    </li>
+                  </ul>
+               </div>
+
+            </div>
+           
+            
+        </nav>
+    </header>
+<!-- 
     <div class="container">
         <label for="searchTitle">Titolo</label>
         <input type="text" v-model="searchTitle" id="searchTitle" name="searchTitle" placeholder="Inserisci il titolo...">
@@ -52,35 +118,129 @@ export default {
     </div>
 
     <div class="container">
-
         <div class="card" v-for="apartment in apartments" @click="$router.push(`/apartments/${apartment.slug}`)">
             <h2>Titolo: {{ apartment.title }}</h2>
             <p>Descrizione: {{ apartment.description }}</p>
         </div>
 
     </div>
-    <button @click="loadMore()">LOAD MORE</button>
+    <button @click="loadMore()">LOAD MORE</button> -->
 </template>
 
 <style lang="scss" scoped>
-h2 {
-    font-size: 1rem;
 
+header {
+    background-color: white;
+    padding: 18px 50px;
+    border-bottom: 1px solid #e2dbdb;
+
+    .navbar {
+      
+        .container-fluid {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .searchbar {
+                .group-list, .group-list:last-child {
+                    border: 1px solid  #B8B8B8;
+                    padding: 12px 25px;
+                    border-radius: 50px;
+                    text-align: center;
+                    -webkit-box-shadow: 1px 2px 9px -1px #B8B8B8; 
+                    box-shadow: 1px 2px 9px -1px #B8B8B8;
+
+                    .item {
+                        margin-right: 2px;
+                    }
+                     .line {
+                        display: inline-block;
+                        color: black;
+                        background-color: black;
+                        width: 20px;
+                        height: 1px;
+                        transform: rotate(90deg);
+                        vertical-align: middle;
+                   }
+
+                   .icon {
+                      border: 1px solid none;
+                      padding: 6px 9px;
+                      border-radius: 50px;
+                      background-color: #ff4a86;
+
+                      .myicon {
+                        color: white;
+                      }
+                   }
+                    
+                }
+            }
+ 
+            .right-menu {
+                width: calc(100% / 4);
+                display: flex;
+                justify-content: center;
+                align-items: baseline;
+
+                .globe {
+                    color: white;
+                    background-color: black;
+                    border-radius: 50px;
+                    font-size: 16px;
+                    vertical-align: middle;
+                 
+                }
+                .group-list:last-child {
+                    border: 1px solid  #e0dcdc;
+                    border-radius: 50px;
+                    padding: 6px 4px;
+                    margin-left: 18px;
+
+                    .user {
+                        font-size: 28px;
+                        color: #6d6a6a;
+                    }
+
+                    .bars {
+                        font-size: 15px;
+                        padding-left: 10px;
+                    }
+                }
+            }
+            .item {
+                display: inline-block;
+                margin-right: 10px;
+                font-size: 14px;
+                font-weight: lighter, bolder;
+                color: black;
+                vertical-align: middle;
+
+                a {
+                    color: black;
+                }
+            }
+        }
+    }
 }
 
-.container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 1.5rem;
+// h2 {
+//     font-size: 1rem;
+// }
 
-    font-size: 0.8rem;
-}
+// .container {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-wrap: wrap;
+//     gap: 1.5rem;
 
-.card {
-    width: 200px;
-    padding: 1rem;
-    border: 2px solid lightblue;
-}
+//     font-size: 0.8rem;
+// }
+
+// .card {
+//     width: 200px;
+//     padding: 1rem;
+//     border: 2px solid lightblue;
+// }
 </style>
