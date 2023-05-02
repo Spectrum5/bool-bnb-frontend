9bf9b34f1391da3578f4caa017697f199248952b
GiadaMarzapane <giada.ortesta@gmail.com>
2023-05-02 20:08:34 +0200

    fix

--- src/views/dashboard/Apartment/ApartmentIndex.vue
+++ src/views/dashboard/Apartment/ApartmentIndex.vue
@@ -21,7 +21,7 @@ export default {
         return {
             router,
             store,
-            // apartments: null,
+            apartments: null,
             selectedApartmentId: null
         }
     },
